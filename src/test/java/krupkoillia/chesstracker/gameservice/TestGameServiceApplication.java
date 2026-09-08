548710757b1b15c6a8a3ca74f33bbe621d9fa2d7
IlliaKrupko <krupkoilla397@gmail.com>
2026-09-09 01:08:57 +0200

    Init commit

---
+++ src/test/java/krupkoillia/chesstracker/gameservice/TestGameServiceApplication.java
@@ -0,0 +1,12 @@
+package krupkoillia.chesstracker.gameservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestGameServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(GameServiceApplication::main).with(TestcontainersConfiguration.class)
+            .run(args);
+    }
+
+}
