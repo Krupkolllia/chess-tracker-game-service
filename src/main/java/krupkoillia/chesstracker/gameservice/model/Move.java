594646329e20ad0367e89727106238f030c57c13
IlliaKrupko <krupkoilla397@gmail.com>
2026-09-11 16:16:43 +0200

    feat(move): create Move entity

---
+++ src/main/java/krupkoillia/chesstracker/gameservice/model/Move.java
@@ -0,0 +1,47 @@
+package krupkoillia.chesstracker.gameservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import krupkoillia.chesstracker.gameservice.model.enums.Color;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Setter
+@Accessors(chain = true)
+@SQLDelete(sql = "UPDATE moves SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
+@Table(name = "moves")
+public class Move {
+
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Id
+    private Long id;
+
+    @Column(name = "move_number", nullable = false)
+    private Integer moveNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Color color;
+
+    @Column(nullable = false)
+    private String fen;
+
+    @Column(nullable = false)
+    private String notation;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+
+}
