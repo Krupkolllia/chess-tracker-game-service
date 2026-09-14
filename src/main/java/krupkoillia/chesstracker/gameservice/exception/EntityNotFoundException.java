4439f722725b06452054efafa62204ac3450c2c0
IlliaKrupko <krupkoilla397@gmail.com>
2026-09-15 01:06:17 +0200

    feat(exception): add EntityNotFoundException

---
+++ src/main/java/krupkoillia/chesstracker/gameservice/exception/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package krupkoillia.chesstracker.gameservice.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
