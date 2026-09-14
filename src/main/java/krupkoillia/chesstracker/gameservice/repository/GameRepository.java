b9d209370e54163eeb4ac5ca8ccde19e05a118b5
IlliaKrupko <krupkoilla397@gmail.com>
2026-09-15 00:43:02 +0200

    feat(game): add useful methods to game repository

--- src/main/java/krupkoillia/chesstracker/gameservice/repository/GameRepository.java
+++ src/main/java/krupkoillia/chesstracker/gameservice/repository/GameRepository.java
@@ -1,8 +1,19 @@
 package krupkoillia.chesstracker.gameservice.repository;
 
-import krupkoillia.chesstracker.gameservice.model.Game;
+import java.util.Optional;
+import krupkoillia.chesstracker.gameservice.model.GameEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameRepository extends JpaRepository<Game, Long> {
+public interface GameRepository extends JpaRepository<GameEntity, Long> {
+
+    Page<GameEntity> findAllByUserId(Long userId, Pageable pageable);
+
+    boolean existsByIdAndUserId(Long id, Long userId);
+
+    Optional<GameEntity> findByIdAndUserId(Long id, Long userId);
+
+    void deleteByIdAndUserId(Long id, Long userId);
 
 }
