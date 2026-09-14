aa206896d7d24c94a81fdbaeec3a94ecd03fca93
IlliaKrupko <krupkoilla397@gmail.com>
2026-09-15 01:29:18 +0200

    refactor(api): set page serialization mode to VIA_DTO

--- src/main/java/krupkoillia/chesstracker/gameservice/GameServiceApplication.java
+++ src/main/java/krupkoillia/chesstracker/gameservice/GameServiceApplication.java
@@ -1,8 +1,12 @@
 package krupkoillia.chesstracker.gameservice;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @SpringBootApplication
 public class GameServiceApplication {
 
