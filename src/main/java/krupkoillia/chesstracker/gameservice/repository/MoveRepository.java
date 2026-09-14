f3b030868f3405fef9aa7b6642188762ac26a9ad
IlliaKrupko <krupkoilla397@gmail.com>
2026-09-15 00:36:44 +0200

    refactor(move): remove redundant move  repository

--- src/main/java/krupkoillia/chesstracker/gameservice/repository/MoveRepository.java
+++
@@ -1,8 +0,0 @@
-package krupkoillia.chesstracker.gameservice.repository;
-
-import krupkoillia.chesstracker.gameservice.model.Move;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MoveRepository extends JpaRepository<Move, Long> {
-
-}
